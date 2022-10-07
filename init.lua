4194122933aaefec7f939c94ef8155b5fa81093e
Samuel Matzko <matzko+sam@gmail.com>
2022-10-07 17:04:21 -0400

    🎨 Simplified the seats, and finished seating positions and views.

--- init.lua
+++ init.lua
@@ -5,33 +5,82 @@ advtrains.register_wagon("green_subway_wagon", {
 		"green_subway_wagon_interior.png",
 		"green_subway_wagon_door.png",
 		"green_subway_wagon_seat.png",
-		"green_subway_wagon_seat.png",
-		"green_subway_wagon_seat.png",
-		"green_subway_wagon_seat.png",
-		"green_subway_wagon_seat.png",
-		"green_subway_wagon_seat.png",
-		"green_subway_wagon_seat.png",
-		"green_subway_wagon_seat.png",
-		"green_subway_wagon_seat.png",
-		"green_subway_wagon_seat.png",
-		"green_subway_wagon_seat.png",
-		"green_subway_wagon_seat.png"
 	},
     drives_on={default=true},
     max_speed=15,
     seats={
 		{
 			name="Driver stand",
-			attach_offset={x=0, y=0, z=0},
-			view_offset={x=0, y=0, z=0},
+			attach_offset={x=-4, y=0.5, z=18},
+			view_offset={x=0, y=0.5, z=0},
 			group="driver_stand",
 		},
         {
 			name="1",
-			attach_offset={x=-4, y=-2, z=8},
-			view_offset={x=0, y=0, z=0},
+			attach_offset={x=-4, y=0.5, z=10},-- 10
+			view_offset={x=0, y=0.5, z=0},
 			group="passenger",
-		}
+		},
+		{
+			name="2",
+			attach_offset={x=-4, y=0.5, z=8},
+			view_offset={x=0, y=0.5, z=0},
+			group="passenger",
+		},
+		{
+			name="3",
+			attach_offset={x=-4, y=0.5, z=0},
+			view_offset={x=0, y=0.5, z=0},
+			group="passenger",
+		},
+		{
+			name="4",
+			attach_offset={x=-4, y=0.5, z=-8},
+			view_offset={x=0, y=0.5, z=0},
+			group="passenger",
+		},
+		{
+			name="5",
+			attach_offset={x=-4, y=0.5, z=-18},
+			view_offset={x=0, y=0.5, z=0},
+			group="passenger",
+		},
+		{
+			name="6",
+			attach_offset={x=-4, y=0.5, z=-24},
+			view_offset={x=0, y=0.5, z=0},
+			group="passenger",
+		},
+		{
+			name="7",
+			attach_offset={x=-4, y=0.5, z=8},
+			view_offset={x=0, y=0.5, z=0},
+			group="passenger",
+		},
+		{
+			name="8",
+			attach_offset={x=-4, y=0.5, z=0},
+			view_offset={x=0, y=0.5, z=0},
+			group="passenger",
+		},
+		{
+			name="9",
+			attach_offset={x=-4, y=0.5, z=-8},
+			view_offset={x=0, y=0.5, z=0},
+			group="passenger",
+		},
+		{
+			name="10",
+			attach_offset={x=-4, y=0.5, z=-18},
+			view_offset={x=0, y=0.5, z=0},
+			group="passenger",
+		},
+		{
+			name="11",
+			attach_offset={x=-4, y=0.5, z=-24},
+			view_offset={x=0, y=0.5, z=0},
+			group="passenger",
+		},
     },
     seat_groups = {
 		driver_stand={
