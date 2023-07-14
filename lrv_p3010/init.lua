8211c01564600727fde2d5b9da94ded97d8ff3d5
Samuel Matzko <matzko+sam@gmail.com>
2023-07-13 20:50:48 -0400

    Finished livery supportgit add .

--- lrv_p3010/init.lua
+++ lrv_p3010/init.lua
@@ -3,6 +3,111 @@ local use_attachment_patch = advtrains_attachment_offset_patch and advtrains_att
 local use_advtrains_livery_designer = minetest.get_modpath("advtrains_livery_designer") and advtrains_livery_designer
 local mod_name = "subways_lrv_p3010"
 
+-- The livery template definition
+local livery_template = {
+    name = "LRV P3010",
+    designer = "Sylvester Kruin",
+    texture_license = "CC-BY-SA-3.0",
+    texture_creator = "Sylvester Kruin",
+    notes = "This template supports independent color overrides for the exterior accents and seats.",
+    base_textures = {
+        "p3010_coupler.png",
+        "p3010_doors.png",
+        "p3010_seats.png",
+        "p3010_undercarriage.png",
+        "p3010_exterior.png",
+        "p3010_interior.png",
+        "p3010_wheels.png"
+    },
+    overlays = {
+        [1] = {name = "Exterior Accents", slot_idx = 5, texture = "p3010_exterior_overlay.png", alpha = 255},
+        [2] = {name = "Seat Accents", slot_idx = 3, texture = "p3010_seats_overlay.png", alpha = 255},
+    }
+}
+
+-- Predefined liveries
+local predefined_livery = {
+    name = "Standard Yellow",
+    notes = "The default yellow color scheme",
+    livery_design = {
+        livery_template_name = "LRV P3010",
+        overlays = {
+            [1] = {id = 1, color = "#FFFF00"},
+            [2] = {id = 2, color = "#E5C21C"},
+        }
+    }
+}
+
+-- If the livery mod is installed, register the wagon with it
+if use_advtrains_livery_designer then
+
+    -- The specific wagon name
+    local wagon_type = "advtrains:lrv_p3010"
+
+    -- This function is called whenever the user presses the "Apply" button
+    local function apply_wagon_livery_textures(player, wagon, textures)
+        if wagon and textures and textures[1] then
+            local data = advtrains.wagons[wagon.id]
+            data.livery = textures[5]
+            data.seat_livery = textures[3]
+            wagon:set_textures(data)
+        end
+    end
+
+    -- Register this mod and its livery function with the advtrains_livery_designer tool
+    advtrains_livery_designer.register_mod(mod_name, apply_wagon_livery_textures)
+
+    -- Register this particular wagon
+    advtrains_livery_database.register_wagon(wagon_type, mod_name)
+
+    -- Add the template and template overlays
+    advtrains_livery_database.add_livery_template(
+        wagon_type,
+        livery_template.name,
+        livery_template.base_textures,
+        mod_name,
+        (livery_template.overlays and #livery_template.overlays) or 0,
+        livery_template.designer,
+        livery_template.texture_license,
+        livery_template.texture_creator,
+        livery_template.notes
+    )
+    if livery_template.overlays then
+        for overlay_id, overlay in ipairs(livery_template.overlays) do
+            advtrains_livery_database.add_livery_template_overlay(
+                wagon_type,
+                livery_template.name,
+                overlay_id,
+                overlay.name,
+                overlay.slot_idx,
+                overlay.texture,
+                overlay.alpha
+            )
+        end
+    end
+
+    -- Register this mod's predefined wagon liveries
+    local livery_design = predefined_livery.livery_design
+    livery_design.wagon_type = wagon_type
+    advtrains_livery_database.add_predefined_livery(
+        predefined_livery.name,
+        livery_design,
+        mod_name,
+        predefined_livery.notes
+    )
+end
+
+-- This function updates the livery when the train is punched
+local function update_livery(self, puncher)
+    local itemstack = puncher:get_wielded_item()
+    local item_name = itemstack:get_name()
+    if use_advtrains_livery_designer and item_name == advtrains_livery_designer.tool_name then
+        advtrains_livery_designer.activate_tool(puncher, self, mod_name)
+        return true
+    end
+    return false
+end
+
 -- The definitions for registering the wagon with advtrains
 local subway_wagon_def = {
     mesh = "p3010.b3d",
@@ -21,20 +126,16 @@ local subway_wagon_def = {
     current_light_texture = "",
     light_texture_forwards = "p3010_light_forwards.png",
     light_texture_backwards = "p3010_light_backwards.png",
+    livery = "",
 
-    -- This function is used to update the textures of the train based on a data table
-    set_textures = function(self)
-        self.object:set_properties({
-            textures = {
-                "p3010_coupler.png",
-                "p3010_doors.png",
-                "p3010_seats.png",
-                "p3010_undercarriage.png",
-                "p3010_exterior.png"..self.current_light_texture,
-                "p3010_interior.png",
-                "p3010_wheels.png",
-            }
-        })
+    -- This function checks if the train is being punched by the livery tool and, if so, activates it
+    custom_may_destroy = function(self, puncher, tiem_from_last_punch, tool_capabilities, direction)
+        return not update_livery(self, puncher)
+    end,
+
+    -- This function is called whenever the train is updated
+    custom_on_step = function(self, dtime, data, train)
+        self:update_textures()
     end,
 
     -- This function is called when the train's velocity changes
@@ -54,9 +155,53 @@ local subway_wagon_def = {
         end
     end,
 
-    -- This function is called whenever the train is updated
-    custom_on_step = function(self, dtime, data, train)
-        self:set_textures()
+    -- Add optional support for the bike painter, in case users don't want to use the livery mod
+    set_livery = function(self, puncher, itemstack, data)
+        local meta = itemstack:get_meta()
+        local color = meta:get_string("paint_color")
+        local alpha = tonumber(meta:get_string("alpha"))
+        if color and color:find("^#%x%x%x%x%x%x$") then
+            data.livery = self.base_texture.."^("..self.base_livery.."^[colorize:"..color..":255)"
+            self:set_textures(data)
+        end
+    end,
+
+    -- This function is used to update the textures of the train based on a data table
+    set_textures = function(self, data)
+        if data.livery then
+            self.livery = data.livery
+            self.seat_livery = data.seat_livery
+            self:update_textures()
+        end
+    end,
+
+    -- This function updates the textures based on the texture variables
+    update_textures = function(self)
+        if self.livery ~= "" then
+            self.object:set_properties({
+                textures = {
+                    "p3010_coupler.png",
+                    "p3010_doors.png",
+                    self.seat_livery,
+                    "p3010_undercarriage.png",
+                    self.livery..self.current_light_texture,
+                    "p3010_interior.png",
+                    "p3010_wheels.png",
+                }
+            })
+        else
+            self.object:set_properties({
+                textures = {
+                    "p3010_coupler.png",
+                    "p3010_doors.png",
+                    "p3010_seats.png",
+                    "p3010_undercarriage.png",
+                    "p3010_exterior.png"..self.current_light_texture,
+                    "p3010_interior.png",
+                    "p3010_wheels.png",
+                }
+            })
+        end
     end,
 
     -- Train physical/interaction configuration
@@ -67,6 +212,7 @@ local subway_wagon_def = {
     coupler_types_back = {p3010 = true},
     coupler_types_front = {Tomlinson = true},
     drives_on = {default = true},
+    drops = {"default:steelblock 4"},
     doors = {
         open = {
             [-1] = {frames = {x = 1, y = 20}, time = 1},
@@ -80,6 +226,7 @@ local subway_wagon_def = {
     is_locomotive = true,
     max_speed = 15,
     wagon_span = 3.15,
+    wheel_positions = {2.1, -3},
 
     -- Seat/user configuration
     assign_to_seat_group = {"test", "passenger", "driver_stand"},
